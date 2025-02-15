1263bfbb91da91dc8a57d6cf40eb9bce60edfae9
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-02-15 10:13:39 +0430

    keyboard disabling after game over

--- src/App.jsx
+++ src/App.jsx
@@ -77,6 +77,7 @@ export default function App() {
         className={className}
         onClick={() => handleKeyboardClick(letter)}
         key={index}
+        disabled={isGameOver}
       >
         {letter.toUpperCase()}
       </button>
